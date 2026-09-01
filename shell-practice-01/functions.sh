578d64c0030bc5583e7d232821b341d6552bb86c
Sai-98-git <thotasaiprasad180@gmail.com>
2026-09-01 11:06:54 +0530

    new

--- shell-practice-01/functions.sh
+++ shell-practice-01/functions.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-greet() {
-    echo "hello"
+
+check_file(){
+    if [ -f "$1" ]
+    then
+        echo "$1 file exits"
+        return 0
+    else
+        echo "$1 file doesnot exits"
+        return 1
+    fi
 }
 
-greet
+check_file "/etc/profile"
+echo "$?"
+check_file "/etc/passwd"
+echo "$?"
