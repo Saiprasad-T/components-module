e81674980a9e2321fa5c5b676ad5cef5f51c7704
Sai-98-git <thotasaiprasad180@gmail.com>
2026-09-04 16:21:34 +0530

    new

---
+++ shell-practice-01/disk_usage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Disk_uasage=$(cd / | df -h | awk 'NR>1 {print $1,$6}')
+
+if [ $Disk_uasage -gt 80 ]
+then
+    echo "Disk Usage: "$Disk_uasage""
+    echo "Warning Disk usage is above 80%"
+else
+    echo "Disk Usage: $Disk_usage""
+    echo "Disk Usage is normal"
+fi 
