f4e8a1b3aed2361309065fca09a2d21356d13f5f
Sai-98-git <thotasaiprasad180@gmail.com>
2026-09-07 14:38:10 +0530

    new

---
+++ shell-practice-01/newlog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#argument validation
+
+
+if [-z "$@" ]
+then
+    echo "please provide log file path"
+fi
+
+if [ -f "$@" ]
+then
+    echo "log file exists"
+else
+    echo "log file doesnot exists"
+fi
+
